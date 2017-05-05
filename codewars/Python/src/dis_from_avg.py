ceb93894f3bef8f8f57150128d8683d14fb61810
MarcMBPDev <marc.wenicker@gmail.com>
2017-05-04 20:26:50 -0700

    add tests to break code, add fix for rounding; submit as kata solution

--- codewars/Python/src/dis_from_avg.py
+++ codewars/Python/src/dis_from_avg.py
@@ -1,9 +1,10 @@
 """Module for solution to https://www.codewars.com/kata/distance-from-the-average."""
 
 from operator import truediv
+from decimal import Decimal
 
 
 def distances_from_average(test_list):
     """Return a list of distances to the average in absolute terms."""
     avg = truediv(sum(test_list), len(test_list))
-    return [float('{}'.format((v - avg) * - 1)) for v in test_list]
+    return [round(float((v - avg) * - 1), 2) for v in test_list]
