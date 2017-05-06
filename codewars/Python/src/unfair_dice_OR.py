4e085587abb12989bfae4c4b523ee09135fc81c1
MarcMBPDev <marc.wenicker@gmail.com>
2017-05-06 15:45:20 -0700

    add sanitization for input and correct return of if an issue with input data exists

--- codewars/Python/src/unfair_dice_OR.py
+++ codewars/Python/src/unfair_dice_OR.py
@@ -4,11 +4,13 @@
 def mutually_exclusive(d6, n1, n2):
     """Return 2 strings, the first containing the probability of either n1 or n2
     being rolled and the second stating the expected outcome or issue with the input."""
-    pass
+    out = sanitize_input(d6)
+    if out:
+        return (None, out)
 
 
 def sanitize_input(d6):
     """Return an alert based on issues with dataset. Return None if no issue."""
     p = sum([l[1] for l in d6])
     if round(p, 5) != 1:
-        return "Check the total probability")
+        return "Check the total probability"
