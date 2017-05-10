aeb2d66b60e56bf5a4cdf148607b040f247141af
MarcMBPDev <marc.wenicker@gmail.com>
2017-05-10 14:31:09 -0700

    remove unnecessary imports, add round to avg and remove unnecessary use of enumerate

--- codewars/Python/src/mean_stddev_of_distance.py
+++ codewars/Python/src/mean_stddev_of_distance.py
@@ -4,16 +4,14 @@ https://www.codewars.com/kata/ad2070-help-lorimar-troubleshoot-his-robots-ultras
 
 from numpy import mean, var
 from math import sqrt
-from operator import truediv
-from decimal import Decimal
 
 
 def sensor_analysis(sensor_data):
     """Return a tuple with the mean and standard deviation of the distance
     variables rounded to four decimal places. The variance should be computed
     using the formula for samples (dividing by N-1)."""
-    distances = [data[1] for idx, data in enumerate(sensor_data)]
-    avg = mean(distances)
+    distances = [data[1] for data in sensor_data]
+    avg = round(mean(distances), 4)
     variance = round(var(distances, ddof=1), 4)
     stddev = round(sqrt(variance), 4)
     return (avg, stddev)
