75110615d2604c26e107762733eee7444a9dc0c5
MarcMBPDev <marc.wenicker@gmail.com>
2017-05-09 18:33:26 -0700

    inital commit https://www.codewars.com/kata/variance-in-a-array-of-words

---
+++ codewars/Python/src/test_var_array_words.py
@@ -0,0 +1,15 @@
+"""Tests for https://www.codewars.com/kata/variance-in-a-array-of-words/python."""
+
+import pytest
+
+
+TEST = [
+    (['Hello', 'world'], 0),
+    (['Hi, world'], 2.25),
+]
+
+@pytest.mark.parametrize("words, result", TEST)
+def test_variance(words, result):
+    """Test that function returns correct var."""
+    from var_array_words import variance
+    assert variance(words) == result
---
+++ codewars/Python/src/var_array_words.py
@@ -0,0 +1,8 @@
+"""Module for solution to https://www.codewars.com/kata/variance-in-a-array-of-words/python."""
+
+
+def variance(words):
+    """Consider X as the aleatory variable that count the number of letters in a
+    word. Write a function that, give in input an array of words (strings),
+    calculate the variance of X. Max decimal of the variance : 4"""
+    pass
