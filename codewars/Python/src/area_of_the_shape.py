725b20854b6573f566d9f715fb52159dcabd8b83
MarcMBPDev <marc.wenicker@gmail.com>
2017-05-11 22:23:38 -0700

    submitted solution for kata

--- codewars/Python/src/area_of_the_shape.py
+++ codewars/Python/src/area_of_the_shape.py
@@ -1,5 +1,13 @@
 """Module for solution to https://www.codewars.com/kata/area-of-a-shape."""
 
+import numpy as np
+
 def area_of_the_shape(f):
     """Return a number representing the area of an unknown shape inside a given square."""
-    pass
+    count = 0
+    n = 10000
+    for _ in range(n):
+        x = np.random.random()
+        y = np.random.random()
+        if f(x, y): count += 1
+    return 1 * count / n
