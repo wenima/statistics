c09f305b15ac4d97f2b53479c6434cf73af8c18e
MarcMBPDev <marc.wenicker@gmail.com>
2017-05-13 16:55:20 -0700

    add calc for slope

--- codewars/Python/src/linear_regression.py
+++ codewars/Python/src/linear_regression.py
@@ -1,17 +1,11 @@
 """Module for solution to https://www.codewars.com/kata/linear-regression-of-y-on-x."""
 
-from numpy import mean
+import numpy as np
 from math import sqrt
 
-
-def avg_var_stdev(dataset):
-    """Return a tuple with 3 values, mean, var and standard deviation based on the input.
-    The input is a list/array."""
-    avg = mean(dataset)
-    variance = sum([(dp[0] - avg) ** 2 for dp in dataset])
-    stddev = sqrt(variance)
-    return (avg, variance, stddev)
-
 def regressionLine(x, y):
-    """ Return the a (intercept) and b (slope) of Regression Line (Y on X)."""
-    pass
+    """Return a tuple of intercept and slope of a given line x, y."""
+    x_deltas = np.diff(x)
+    y_deltas = np.diff(y)
+    slopes = [y_delta/x_delta for x_delta, y_delta in zip(x_deltas, y_deltas)]
+    
