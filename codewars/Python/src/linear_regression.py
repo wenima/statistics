ae5ee92e373a5583008e21213bb5cebfa3c919c0
MarcMBPDev <marc.wenicker@gmail.com>
2017-05-13 19:17:22 -0700

    solve for formula for slope and intercept

--- codewars/Python/src/linear_regression.py
+++ codewars/Python/src/linear_regression.py
@@ -1,11 +1,13 @@
 """Module for solution to https://www.codewars.com/kata/linear-regression-of-y-on-x."""
 
 import numpy as np
-from math import sqrt
+from math import sqrt, ceil
+import operator as op
 
 def regressionLine(x, y):
-    """Return a tuple of intercept and slope of a given line x, y."""
-    x_deltas = np.diff(x)
-    y_deltas = np.diff(y)
-    slopes = [y_delta/x_delta for x_delta, y_delta in zip(x_deltas, y_deltas)]
-    
+    """Return a tuple of intercept and slope of a given line x_coords, y_coords."""
+    sum_xy = sum([xy for xy in map(op.mul, x, y)])
+    sum_x_squared = sum([n ** 2 for n in x])
+    slope = (len(x) * sum_xy - sum(x) * sum(y)) / (len(x) * sum_x_squared - sum(x) ** 2)
+    intercept = (sum(y) - slope * sum(x)) / len(x)
+    return float('{0:.4f}'.format(intercept)), float('{0:.4f}'.format(slope))
---
+++ codewars/Python/src/test_linear_regression.py
@@ -0,0 +1,16 @@
+"""Tests for https://www.codewars.com/kata/linear-regression-of-y-on-x"""
+
+import pytest
+
+
+TEST = [
+    ([25,30,35,40,45,50], [78,70,65,58,48,42], (114.381, -1.4457)),
+    ([56,42,72,36,63,47,55,49,38,42,68,60], [147,125,160,118,149,128,150,145,115,140,152,155], (80.7777, 1.138)),
+]
+
+
+@pytest.mark.parametrize("x, y, result", TEST)
+def test_regressionLine(x, y, result):
+    """Test that function returns correct tuple of intercept and slope."""
+    from linear_regression import regressionLine
+    assert regressionLine(x, y) == result
