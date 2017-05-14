c7a10d2786690bfe88ba3d2d6cbe301b97c7824e
MarcMBPDev <marc.wenicker@gmail.com>
2017-05-13 21:04:12 -0700

    initial commit for https://www.codewars.com/kata/probabilities-for-sums-in-rolling-cubic-dice

---
+++ codewars/Python/src/p_sums_cubic_dice.py
@@ -0,0 +1,8 @@
+"""Module for solution to https://www.codewars.com/kata/probabilities-for-sums-in-rolling-cubic-dice."""
+
+
+
+def rolldice_sum_prob(target_sum, no_of_dice):
+    """Return a number representing the probability of rolling a number equal to
+    target_sum with a given number of dice."""
+    pass
---
+++ codewars/Python/src/test_p_sums_cubic_dice.py
@@ -0,0 +1,17 @@
+"""Tests for https://www.codewars.com/kata/probabilities-for-sums-in-rolling-cubic-dice"""
+
+import pytest
+
+TEST = [
+    (11, 2, 0.0555555555),
+    (8, 2, 0.1388888888),
+    (8, 3, 0.0972222222222),
+    (22, 3, 0),
+]
+
+
+@pytest.mark.parametrize("target_sum, no_of_dice, result", TEST)
+def test_rolldice_sum_prob(target_sum, no_of_dice, result):
+    """Test that function returns correct tuple of intercept and slope."""
+    from p_sums_cubic_dice import rolldice_sum_prob
+    assert rolldice_sum_prob(target_sum, no_of_dice) == result
