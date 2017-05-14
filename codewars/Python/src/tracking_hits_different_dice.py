3dd0d5ef7b6142460880f692f53da40acf8f928d
MarcMBPDev <marc.wenicker@gmail.com>
2017-05-14 15:09:27 -0700

    add code to pass basic test

--- codewars/Python/src/tracking_hits_different_dice.py
+++ codewars/Python/src/tracking_hits_different_dice.py
@@ -5,4 +5,9 @@ from itertools import product
 def reg_sum_hits(no_of_dice, sides):
     """Return a list of list containing the sum value and the number of hits for
     given number of dice of given sides."""
-    pass
+    die = [x for x in range(1, sides + 1)]
+    dice = [die for _ in range(no_of_dice)]
+    hits = []
+    for sum_roll in range(no_of_dice, no_of_dice * sides + 1):
+        hits.append([sum_roll, len([pr for pr in product(*dice) if sum(pr) == sum_roll])])
+    return hits
