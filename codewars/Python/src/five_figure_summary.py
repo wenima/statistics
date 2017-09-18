fdbeae3b3ce73a1098062f6ab96de4b60ee50faf
MarcMBPDev <marc.wenicker@gmail.com>
2017-09-18 13:32:59 -0700

    pass basic test

--- codewars/Python/src/five_figure_summary.py
+++ codewars/Python/src/five_figure_summary.py
@@ -8,10 +8,33 @@ class StatisticalSummary(object):
 
     def __init__(self, seq):
         self.seq = seq
+        self.seq = sorted([x for x in seq])
 
     def five_figure_summary(self, precision=None):
-        """The five figure summary gives the lower extreme, upper extreme, lower
-        quartile, median, and upper quartile, all derived from the supplied sample
-        data.
+        """Return number of valid samples, lower extreme, upper extreme, lower
+        quartile, median, and upper quartile.
         """
-        
+
+        n = len(self.seq)
+        low = min(self.seq)
+        high = max(self.seq)
+
+        median_idx = median_idx = (len(self.seq) - 1) // 2
+        if not len(self.seq) % 2:
+            median = (self.seq[median_idx] + self.seq[median_idx + 1]) / 2
+        else:
+            median = self.seq[median_idx]
+
+        lq_idx = int(median_idx) // 2
+        if len(self.seq) % 2:
+            lq = (self.seq[lq_idx] + self.seq[lq_idx + 1]) / 2
+        else:
+            lq = self.seq[lq_idx]
+
+        uq_idx = (median_idx + len(self.seq) - 1) // 2
+        if len(self.seq) % 2:
+            uq = (self.seq[uq_idx] + self.seq[uq_idx + 1]) / 2
+        else:
+            uq = self.seq[uq_idx]
+
+        return (n, low, high, lq, median, uq)
