1aac3ad8c25a5119853951fbd0f7ca79fbcb72b9
MarcMBPDev <marc.wenicker@gmail.com>
2017-09-21 21:44:04 -0700

    refactor code to make getting quartile generic and accept parameter q

--- codewars/Python/src/five_figure_summary.py
+++ codewars/Python/src/five_figure_summary.py
@@ -1,5 +1,6 @@
-"""Tests for https://www.codewars.com/kata/intro-to-statistics-part-1-a-five-figure-summary/python."""
+"""Solution for https://www.codewars.com/kata/intro-to-statistics-part-1-a-five-figure-summary/python."""
 
+from math import modf
 
 class StatisticalSummary(object):
     """Class which contains a method to return six figure version of the five number summary
@@ -9,32 +10,30 @@ class StatisticalSummary(object):
     def __init__(self, seq):
         self.seq = seq
         self.seq = sorted([x for x in seq])
+        self.n = len(self.seq)
 
     def five_figure_summary(self, precision=None):
         """Return number of valid samples, lower extreme, upper extreme, lower
         quartile, median, and upper quartile.
         """
-
-        n = len(self.seq)
         low = min(self.seq)
         high = max(self.seq)
 
-        median_idx = median_idx = (len(self.seq) - 1) // 2
-        if not len(self.seq) % 2:
-            median = (self.seq[median_idx] + self.seq[median_idx + 1]) / 2
-        else:
-            median = self.seq[median_idx]
+        return (self.n, low, high, self._get_quartile(25), self._get_quartile(50), self._get_quartile(75))
 
-        lq_idx = int(median_idx) // 2
-        if len(self.seq) % 2:
-            lq = (self.seq[lq_idx] + self.seq[lq_idx + 1]) / 2
+    def _get_quartile(self, q):
+        """Return the 25th, 50th or 75th based on Freund and Perles's method."""
+        n = self.n
+        if q == 50:
+            idx = idx = (n - 1) // 2
+            if not n % 2:
+                return (self.seq[idx] + self.seq[idx + 1]) / 2
+            else:
+                return self.seq[idx]
+        elif q == 25:
+            idx = ((n + 3) / 4) - 1
         else:
-            lq = self.seq[lq_idx]
-
-        uq_idx = (median_idx + len(self.seq) - 1) // 2
-        if len(self.seq) % 2:
-            uq = (self.seq[uq_idx] + self.seq[uq_idx + 1]) / 2
-        else:
-            uq = self.seq[uq_idx]
-
-        return (n, low, high, lq, median, uq)
+            idx = (3 * n + 1) / 4 - 1
+        frac, idx = modf(idx)
+        idx = int(idx)
+        return self.seq[idx] + (self.seq[idx + 1] - self.seq[idx]) * frac
