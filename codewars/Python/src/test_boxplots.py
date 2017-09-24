73a5bae6fa80645713b6f5d75c7d0580ea7a6932
MarcMBPDev <marc.wenicker@gmail.com>
2017-09-24 14:25:41 -0700

    add test for basic boxplot

--- codewars/Python/src/test_boxplots.py
+++ codewars/Python/src/test_boxplots.py
@@ -3,6 +3,9 @@
 import pytest
 
 BOXPLOT = 'BOXPLOT'
+BOX_AND_WHISKER = 'BOX_AND_WHISKER'
+BOX_AND_DECILE_WHISKER = 'BOX_AND_DECILE_WHISKER'
+TUKEY_BOX_AND_WHISKER = 'TUKEY_BOX_AND_WHISKER'
 
 SEQUENCE = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
@@ -21,6 +24,13 @@ PERCENTILES = [
     (.9, 26.1),
 ]
 
+BOXPLOTS = [
+    (BOXPLOT, SEQUENCE, ('Sample', 12.75, 16, 20)),
+    # (BOX_AND_WHISKER, SEQUENCE, ('Sample', 1, 12.75, 16, 20, 32)),
+    # (BOX_AND_DECILE_WHISKER, SEQUENCE, ('Sample', [1, 2, 3, 4, 5, 6], 6.9, 12.75, 16, 20, 26.1, [27, 28, 29, 30, 31, 32])),
+    # (TUKEY_BOX_AND_WHISKER, SEQUENCE, ('Sample', [1, 2, 3, 4, 5], 6, 12.75, 16, 20, 26, [27, 28, 29, 30, 31, 32])),
+]
+
 
 @pytest.mark.parametrize("p, result", PERCENTILES)
 def test_percentile(p, result):
@@ -30,3 +40,10 @@ def test_percentile(p, result):
     from boxplots import StatisticalSummary
     stats_summary = StatisticalSummary(SEQUENCE)
     assert stats_summary._percentile(p) == result
+
+@pytest.mark.parametrize("boxplot_type, seq, result", BOXPLOTS)
+def test_boxplot(boxplot_type, seq, result):
+    """Test that boxplot returns correct sample data based on type of boxplot passed in."""
+    from boxplots import StatisticalSummary
+    stats_summary = StatisticalSummary(seq)
+    assert stats_summary.boxplot(boxplot_type) == result
